a7848246e7df8d56931774811160d44e59091cd6
engciiltire <mnuur2196@gmail.com>
2026-06-18 04:56:24 -0700

    react: 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Greeting } from "./exercis-1/Greeting"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Greeting/>
+    </>
+
+  )
+    
+
+}
+
+export default App
---
+++ src/exercis-1/Greeting.jsx
@@ -0,0 +1,12 @@
+export const Greeting=()=>{
+    return(
+        <>
+        <h1>name: mohamud osman xaamuud</h1>
+        <h2>loction : afgoye </h2>
+        <h2>xaarada : xaawo tako </h2>
+        <h3> qaasatan suuq yaraha </h3>
+        <p>mcdev@gmail.com</p>
+       
+        </>
+    )
+}
