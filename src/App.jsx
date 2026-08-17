1093399f71375b8cc950bc667aa054635b3fc44b
engciiltire <mnuur2196@gmail.com>
2026-08-17 00:37:51 -0700

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import { useLanguage } from "./exercise-15/LanguageContext";
+import { useCart } from "./exercise-16/CartContext";
 
 function App() {
-  const { texts, toggleLanguage } = useLanguage();
+  const products = [
+    {
+      id: 1,
+      name: "Widget",
+      price: 19.99,
+    },
+    {
+      id: 2,
+      name: "Gadget",
+      price: 29.99,
+    },
+  ];
+
+  const { cart, addToCart, removeFromCart } = useCart();
 
   return (
     <div>
-      <button onClick={toggleLanguage}>
-        {texts.button}
-      </button>
+      {/* Products */}
+      {products.map((product) => (
+        <div key={product.id}>
+          <h3>{product.name}</h3>
 
-      <h1>{texts.greeting}</h1>
+          <p>Price: ${product.price}</p>
+
+          <button onClick={() => addToCart(product)}>
+            Add to Cart
+          </button>
+        </div>
+      ))}
+
+      {/* Cart */}
+      <h1>Cart Summary</h1>
+
+      <p>Total Items: {cart.length}</p>
+
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+
+            <button onClick={() => removeFromCart(item.id)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { LanguageProvider } from "./exercise-15/LanguageContext";
+import { CartProvider } from "./exercise-16/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LanguageProvider>
+    <CartProvider>
       <App />
-    </LanguageProvider>
+    </CartProvider>
   </StrictMode>
 );
